65694701a9c5fb13a246edd11ef36472c2af4e8f
A_xiaozhupo <yu51920@qq.com>
2017-12-27 08:13:53 +0800

    dierci
    /

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by khj on 2017/12/26.
+ */
+$(".top .jiantou").click(function(){
+    $(".bottom").show();
+    $(".banner").hide();
+    $(".footer").hide();
+    $(".top").hide();
+});
+$(".bottom .bottom_D .bottom_DD .DD_right .jiantouu").click(function(){
+    $(".top").show();
+    $(".banner").show();
+    $(".footer").show();
+    $(".bottom").hide();
+})
