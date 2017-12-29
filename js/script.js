b8c74ed54d415cec1e157a30cf8088b20d85a349
A_xiaozhupo <yu51920@qq.com>
2017-12-29 08:37:12 +0800

    Jixu
    1

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
 $(".top .jiantou").click(function(){
     $(".bottom").show();
     $(".banner").hide();
-    $(".footer").hide();
+
     $(".top").hide();
 });
 $(".bottom .bottom_D .bottom_DD .DD_right .jiantouu").click(function(){
     $(".top").show();
     $(".banner").show();
-    $(".footer").show();
+
     $(".bottom").hide();
 })
